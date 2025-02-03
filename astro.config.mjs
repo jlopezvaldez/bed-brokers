535e36d3e306bddbe5cd490faef3413dbbc00a92
jonathan <jlopezv0324@gmail.com>
2025-02-03 13:07:41 -0700

    robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import netlify from '@astrojs/netlify'
-
+import netlify from '@astrojs/netlify';
 import react from "@astrojs/react";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://azbedbrokers.com/',
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), robotsTxt()],
   output: 'server',
   adapter: netlify()
 });
